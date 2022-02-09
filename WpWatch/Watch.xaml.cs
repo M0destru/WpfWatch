a22fcc7eb54ecfd85a06e2c6b0b1a8508df56592
Rustam-PC <Rustam-PC@RUSTAM-PC>
2022-02-09 14:02:26 +0500

    Добавьте файлы проекта.

---
+++ WpWatch/Watch.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace WpfWatch
+{
+    /// <summary>
+    /// Логика взаимодействия для Watch.xaml
+    /// </summary>
+    public partial class Watch : UserControl
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        
+        public Watch()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(0.01);
+            timer.Tick += TimerTick;
+            timer.Start();
+        }
+
+        private void TimerTick (object sender, EventArgs e)
+        {
+            var time = DateTime.Now;
+            RotationSecond.Angle = 6 * (time.Second + time.Millisecond / 1000.0);
+            RotationMinute.Angle = 6 * time.Minute + RotationSecond.Angle / 60;
+            RotationHour.Angle = 30 * (time.Hour % 12) + RotationMinute.Angle / 12;
+        }
+    }
+}
---
+++ WpfWatchApp/WatchWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfWatchApp
+{
+    /// <summary>
+    /// Логика взаимодействия для WatchWindow.xaml
+    /// </summary>
+    public partial class WatchWindow : Window
+    {
+        public WatchWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
